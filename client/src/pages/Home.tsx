4383ef62e1ad0c17b7627f9ef2db7e4d131ba1bd
Deivide Mangos <seuemail@gmail.com>
2026-08-04 05:45:39 -0300

    Atualizações da landing page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -578,7 +578,7 @@ Melhor horário para contato: ${formData.horario}.`;
         </div>
 
         {/* Benefícios */}
-        <div className="space-y-5">
+        <div className="space-y-5 flex flex-col items-center md:items-start">
 
           <div className="flex items-center gap-3">
             <CheckCircle className="w-5 h-5 text-accent" />
